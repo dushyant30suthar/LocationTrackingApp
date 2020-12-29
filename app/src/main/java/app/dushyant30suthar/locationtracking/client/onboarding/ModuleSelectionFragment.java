1acc8e45327f7ba900052767040e8d525187f899
Dushyant Suthar <dushyant30suthar@gmail.com>
2020-12-29 18:41:30 +0530

    Add boilerplate for onboarding client module and nav flow

--- app/src/main/java/app/dushyant30suthar/locationtracking/client/onboarding/ModuleSelectionFragment.java
+++ app/src/main/java/app/dushyant30suthar/locationtracking/client/onboarding/ModuleSelectionFragment.java
@@ -1,4 +1,22 @@
 package app.dushyant30suthar.locationtracking.client.onboarding;
 
-class ModuleSelectionFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import app.dushyant30suthar.locationtracking.R;
+
+class ModuleSelectionFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_module_selection, container, false);
+        return view;
+    }
 }
--- app/src/main/java/app/dushyant30suthar/locationtracking/client/onboarding/OnboardingActivity.java
+++ app/src/main/java/app/dushyant30suthar/locationtracking/client/onboarding/OnboardingActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import app.dushyant30suthar.locationtracking.R;
+
 class OnboardingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_onboarding);
     }
 }
