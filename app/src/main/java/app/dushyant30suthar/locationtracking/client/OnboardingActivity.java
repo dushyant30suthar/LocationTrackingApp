1cfc2558322452c3714c10dd0329d6afcdaa0911
Dushyant Suthar <dushyant30suthar@gmail.com>
2020-12-29 18:14:48 +0530

    Initial commit

---
+++ app/src/main/java/app/dushyant30suthar/locationtracking/client/OnboardingActivity.java
@@ -0,0 +1,15 @@
+package app.dushyant30suthar.locationtracking.client;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/app/dushyant30suthar/locationtracking/client/ProducerActivity.java
@@ -0,0 +1,8 @@
+package app.dushyant30suthar.locationtracking.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+class ProducerActivity extends AppCompatActivity {
+
+
+}
