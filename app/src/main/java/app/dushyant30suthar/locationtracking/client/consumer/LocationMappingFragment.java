4e916ccd4cf5de56f95c16f3ae1d8f159b50b162
Dushyant Suthar <dushyant30suthar@gmail.com>
2020-12-30 22:04:05 +0530

    Add markers on updating location

--- app/src/main/java/app/dushyant30suthar/locationtracking/client/consumer/LocationMappingFragment.java
+++ app/src/main/java/app/dushyant30suthar/locationtracking/client/consumer/LocationMappingFragment.java
@@ -1,14 +1,28 @@
 package app.dushyant30suthar.locationtracking.client.consumer;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
 
@@ -20,9 +34,19 @@ import app.dushyant30suthar.locationtracking.domain.location.locationReceiver.Lo
 public class LocationMappingFragment extends Fragment implements LocationDao.OnUsersLocationUpdateListener {
 
     private LocationReceiverController locationReceiverController;
+    private GoogleMap googleMap;
 
     public LocationMappingFragment() {
     }
+
+    private final OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            LocationMappingFragment.this.googleMap = googleMap;
+        }
+    };
+
 
     @Nullable
     @Override
@@ -40,6 +64,11 @@ public class LocationMappingFragment extends Fragment implements LocationDao.OnU
 
     private void setUpViews(View view) {
         locationReceiverController = LocationReceiverController.getInstance(this);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
     }
 
     private void startWatchingUsers() {
@@ -53,6 +82,56 @@ public class LocationMappingFragment extends Fragment implements LocationDao.OnU
 
     @Override
     public void onUsersLocationUpdate(List<User> userList) {
+        if (googleMap == null)
+            return;
+
         Log.e("updated", userList.get(0).getLatitude() + "");
+        for (User user : userList) {
+
+            /*
+             * That's a very poor logic to filter out the current user from the map.
+             *
+             * current user should be filtered out on the basis of id rather than this
+             * kind of logic.*/
+            if (user.getLatitude() == 0.0 || user.getLongitude() == 0.0) {
+
+            } else {
+                LatLng userLocation = new LatLng(user.getLatitude(), user.getLongitude());
+                googleMap.addMarker(new MarkerOptions().position(userLocation));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+            }
+        }
+    }
+
+    public void animateMarker(final Marker marker, final LatLng toPosition, final boolean hideMarker) {
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        Projection proj = googleMap.getProjection();
+        Point startPoint = proj.toScreenLocation(marker.getPosition());
+        final LatLng startLatLng = proj.fromScreenLocation(startPoint);
+        final long duration = 500;
+
+        final Interpolator interpolator = new LinearInterpolator();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                float t = interpolator.getInterpolation((float) elapsed
+                        / duration);
+                double lng = t * toPosition.longitude + (1 - t)
+                        * startLatLng.longitude;
+                double lat = t * toPosition.latitude + (1 - t)
+                        * startLatLng.latitude;
+                marker.setPosition(new LatLng(lat, lng));
+
+                if (t < 1.0) {
+                    // Post again 16ms later.
+                    handler.postDelayed(this, 16);
+                } else {
+                    marker.setVisible(!hideMarker);
+                }
+            }
+        });
     }
 }
