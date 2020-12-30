5baf9170ec871d8d32751ba22a6715a867c620b7
Dushyant Suthar <dushyant30suthar@gmail.com>
2020-12-30 20:11:36 +0530

    Add methods for starting and stopping usersLocation updates

---
+++ app/src/main/java/app/dushyant30suthar/locationtracking/domain/location/locationReceiver/LocationReceiverController.java
@@ -0,0 +1,29 @@
+package app.dushyant30suthar.locationtracking.domain.location.locationReceiver;
+
+import app.dushyant30suthar.locationtracking.domain.location.LocationDao;
+
+class LocationReceiverController {
+    private static LocationReceiverController locationReceiverController;
+    private final LocationDao.OnUsersLocationUpdateListener onUsersLocationUpdateListener;
+    private final LocationDao locationDao;
+
+    private LocationReceiverController(LocationDao.OnUsersLocationUpdateListener onUsersLocationUpdateListener) {
+        this.onUsersLocationUpdateListener = onUsersLocationUpdateListener;
+        locationDao = new LocationDao();
+    }
+
+    public static LocationReceiverController getInstance(LocationDao.OnUsersLocationUpdateListener onUsersLocationUpdateListener) {
+        if (locationReceiverController == null) {
+            locationReceiverController = new LocationReceiverController(onUsersLocationUpdateListener);
+        }
+        return locationReceiverController;
+    }
+
+    public void startReceivingUsersLocationUpdates() {
+        locationDao.addOnLocationUpdateListener(onUsersLocationUpdateListener);
+    }
+
+    public void stopReceivingUsersLocationUpdatesFor() {
+        locationDao.removeOnLocationUpdateListener();
+    }
+}
